755c3665e4d33d376e968ba3426bf008c6cbf122
wnghks7787 <wnghks7787@naver.com>
2025-08-24 15:31:11 +0900

    add more options

--- docker_compose_builder/initial_page_builder/selenium_builder/joomla_autobuilder.py
+++ docker_compose_builder/initial_page_builder/selenium_builder/joomla_autobuilder.py
@@ -33,6 +33,10 @@ def driver_getter(port_num):
     # Option 설정
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
+    options.add_argument("window-size=1400,1500")
+    options.add_argument("start-maximized")
+    options.add_agrument("enable-automation")
+    options.add_argument("--disable-infobars")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
