6fa9a226d3cae540efaaab9cee51d2334ee0be85
wnghks7787 <wnghks7787@naver.com>
2025-08-24 13:40:40 +0900

    update joomla autobuilder

--- docker_compose_builder/initial_page_builder/selenium_builder/joomla_autobuilder.py
+++ docker_compose_builder/initial_page_builder/selenium_builder/joomla_autobuilder.py
@@ -5,6 +5,12 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import options
+from selenium.webdriver.chrome.service import service
+
+import tempfile
+import shutil
+import os
 
 import time
 import argparse
@@ -12,20 +18,29 @@ import argparse
 STEP_TIME = 1
 INSTALL_TIME = 10
 
+CHROME_DRIVER_PATH = '/home/wnghks7787/chromedriver-linux64/chromedriver'
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--portnum', required=True)
 parser.add_argument('--version', required=True)
 
 args = parser.parse_args()
 
-options = webdriver.ChromeOptions()
-options.add_argument("headless")
-
 def driver_getter(port_num):
-    driver = webdriver.Chrome()
+    # Option 설정
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument(f"--user-data-dir={user_data_dir}")
+
+    service = Service(excutable_path=CHROME_DRIVER_PATH)
+
+    driver = webdriver.Chrome(service=service, options=options)
     driver.get(f'http://localhost:{port_num}')
 
-    return driver
+    return driver, user_data_dir
 
 # STEP1: Select language and site name
 def step1(driver):
@@ -37,17 +52,24 @@ def step1(driver):
 
 # STEP2: Setting Super User
 def step2(driver):
-    super_user_name = driver.find_element(By.NAME, 'jform[admin_user]')
-    super_user_account = driver.find_element(By.NAME, 'jform[admin_username]')
-    super_user_password = driver.find_element(By.NAME, 'jform[admin_password]')
-    super_user_email = driver.find_element(By.NAME, 'jform[admin_email]')
-    step2_button = driver.find_element(By.ID, 'step2')
+    try:
+        super_user_name = driver.find_element(By.NAME, 'jform[admin_user]')
+        super_user_account = driver.find_element(By.NAME, 'jform[admin_username]')
+        super_user_password = driver.find_element(By.NAME, 'jform[admin_password]')
+        super_user_email = driver.find_element(By.NAME, 'jform[admin_email]')
+        step2_button_locator = driver.find_element(By.ID, 'step2')
 
-    super_user_name.send_keys("admin")
-    super_user_account.send_keys("admin")
-    super_user_password.send_keys("password12345678")
-    super_user_email.send_keys("wnghks7787@naver.com")
-    step2_button.click()
+        super_user_name.send_keys("admin")
+        super_user_account.send_keys("admin")
+        super_user_password.send_keys("password12345678")
+        super_user_email.send_keys("wnghks7787@naver.com")
+        step2_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(step2_button_locator)
+        )
+        driver.execute.script("arguments[0].click()", step2_button)
+    except TimeoutException:
+        print("클릭할 수 없습니다.")
+        raise
 
 # STEP3: DB Setting
 def step3(driver):
@@ -70,28 +92,38 @@ def step3(driver):
 
 
 def run():
-    driver = driver_getter(args.portnum)
-
-    step1(driver)
-    time.sleep(STEP_TIME)
-
-    step2(driver)
-    time.sleep(STEP_TIME)
-
-    step3(driver)
+    driver = None
+    user_data_dir_to_clean = None
 
     try:
-        wait = WebDriverWait(driver, 300)
+        driver, user_data_dir_to_clean = driver_getter(args.portnum)
 
-        finish = wait.until(
-            EC.visibility_of_element_located((By.XPATH, '//*[@id="installRecommended"]/div/div[2]/button[1]'))
-        )
-    except TimeoutException:
-        print("error with timeout")
-    
-    time.sleep(STEP_TIME)
+        step1(driver)
+        time.sleep(STEP_TIME)
 
-    driver.quit()
+        step2(driver)
+        time.sleep(STEP_TIME)
+
+        step3(driver)
+
+        try:
+            wait = WebDriverWait(driver, 300)
+
+            finish = wait.until(
+                EC.visibility_of_element_located((By.XPATH, '//*[@id="installRecommended"]/div/div[2]/button[1]'))
+            )
+        except TimeoutException:
+            print("error with timeout")
+        
+        time.sleep(STEP_TIME)
+
+        driver.quit()
+    finally:
+        if driver:
+            driver.quit()
+
+        if user_data_dir_to_clean:
+            shutil.rmtree(user_data_dir_to_clean)
 
 if __name__ == '__main__':
     run()
