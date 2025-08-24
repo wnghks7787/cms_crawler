e337f30535f9f7a5fca36aa722eeb3aa168506b2
wnghks7787 <wnghks7787@naver.com>
2025-08-24 14:04:00 +0900

    find typo

--- docker_compose_builder/initial_page_builder/selenium_builder/joomla_autobuilder.py
+++ docker_compose_builder/initial_page_builder/selenium_builder/joomla_autobuilder.py
@@ -5,8 +5,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.chrome.options import options
-from selenium.webdriver.chrome.service import service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 import tempfile
 import shutil
