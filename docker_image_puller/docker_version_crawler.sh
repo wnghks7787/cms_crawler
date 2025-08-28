651a0105f86c885c87dc86cd4284c6acc9e41cab
wnghks7787 <wnghks7787@naver.com>
2025-08-28 09:34:11 +0900

    update for docker image version X.X.X.X

--- docker_image_puller/docker_version_crawler.sh
+++ docker_image_puller/docker_version_crawler.sh
@@ -13,20 +13,20 @@ do
     result_file=$result_directory"/"$cms"_version"
     mkdir $result_directory
     touch $result_file
+    (
+        while true;
+        do
+            result=$(curl -s "https://hub.docker.com/v2/repositories/$provider/$cms/tags?page_size=100&page=$page")
+            
 
-    while true;
-    do
-        result=$(curl -s "https://hub.docker.com/v2/repositories/$provider/$cms/tags?page_size=100&page=$page")
-        
+            # echo "$result" | jq -r '.results[].name | match("[0-9]+(\\.[0-9]+)+") | .string' >> $result_file
+            echo "$result" | jq -r '.results[].name' | grep -E '^[0-9]+(\.[0-9]+)*$'
 
-        echo "$result" | jq -r '.results[].name' | grep -E '^[0-9]+\.[0-9]+(\.[0-9]+)?$' >> $result_file
-        echo "$result" | jq -r '.results[].name' | grep -E '^[0-9]+\.[0-9]+(\.[0-9]+)?$'
-
-        next=$(echo "$result" | jq -r '.next')
-        if [ "$next" = "null" ]; then
-            break
-        fi
-        page=$((page + 1))
-    done
-
-done < <(tail -n  +1 "$FILE")
+            next=$(echo "$result" | jq -r '.next')
+            if [ "$next" = "null" ]; then
+                break
+            fi
+            page=$((page + 1))
+        done
+    ) | sort -V -u > "$result_file"
+done < <(tail -n  +1 "$FILE") 
