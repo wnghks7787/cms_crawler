fe4d634730ba5851eff0d650ccfb343924b0c444
wnghks7787 <wnghks7787@naver.com>
2025-08-29 13:24:56 +0900

    update version_crawler

--- docker_image_puller/docker_version_crawler.sh
+++ docker_image_puller/docker_version_crawler.sh
@@ -1,32 +1,103 @@
+# #!/bin/bash
+
+# RELATIVE_PATH="../resources"
+# RESOURCES_PATH=${PWD}'/'$RELATIVE_PATH
+# FILE=$RESOURCES_PATH/"docker_hub_library.csv"
+
+# while IFS=',' read -r provider cms;
+# do
+#     echo "Provider: $provider, CMS: $cms"
+
+#     page=1
+#     result_directory=$RESOURCES_PATH"/docker_hub_library_version"
+#     result_file=$result_directory"/"$cms"_version"
+#     mkdir $result_directory
+#     touch $result_file
+#     (
+#         while true;
+#         do
+#             result=$(curl -s "https://hub.docker.com/v2/repositories/$provider/$cms/tags?page_size=100&page=$page")
+            
+
+#             # echo "$result" | jq -r '.results[].name | match("[0-9]+(\\.[0-9]+)+") | .string' >> $result_file
+#             echo "$result" | jq -r '.results[].name' | grep -E '^[0-9]+(\.[0-9]+)*$'
+
+#             next=$(echo "$result" | jq -r '.next')
+#             if [ "$next" = "null" ]; then
+#                 break
+#             fi
+#             page=$((page + 1))
+#         done
+#     ) | sort -V -u > "$result_file"
+# done < <(tail -n  +1 "$FILE") 
+
 #!/bin/bash
 
+# 리소스 파일 경로를 설정합니다.
 RELATIVE_PATH="../resources"
 RESOURCES_PATH=${PWD}'/'$RELATIVE_PATH
 FILE=$RESOURCES_PATH/"docker_hub_library.csv"
 
-while IFS=',' read -r provider cms;
+# CSV 파일의 각 라인을 읽어들입니다.
+# 파이프를 사용하여 tail의 출력을 while 루프로 전달하는 방식으로 수정했습니다.
+tail -n +1 "$FILE" | while IFS=',' read -r provider cms;
 do
-    echo "Provider: $provider, CMS: $cms"
+  # 공백만 있는 라인은 건너뜁니다.
+  if [ -z "$provider" ] || [ -z "$cms" ]; then
+    continue
+  fi
 
-    page=1
-    result_directory=$RESOURCES_PATH"/docker_hub_library_version"
-    result_file=$result_directory"/"$cms"_version"
-    mkdir $result_directory
-    touch $result_file
-    (
-        while true;
-        do
-            result=$(curl -s "https://hub.docker.com/v2/repositories/$provider/$cms/tags?page_size=100&page=$page")
-            
+  echo "Provider: $provider, CMS: $cms"
 
-            # echo "$result" | jq -r '.results[].name | match("[0-9]+(\\.[0-9]+)+") | .string' >> $result_file
-            echo "$result" | jq -r '.results[].name' | grep -E '^[0-9]+(\.[0-9]+)*$'
+  page=1
+  result_directory=$RESOURCES_PATH"/docker_hub_library_version"
+  result_file=$result_directory"/"$cms"_version"
 
-            next=$(echo "$result" | jq -r '.next')
-            if [ "$next" = "null" ]; then
-                break
-            fi
-            page=$((page + 1))
-        done
-    ) | sort -V -u > "$result_file"
-done < <(tail -n  +1 "$FILE") 
+  # 결과 디렉토리가 없으면 생성합니다. (-p 옵션으로 오류 방지)
+  mkdir -p "$result_directory"
+  # 결과 파일을 새로 생성하거나 비웁니다.
+  > "$result_file"
+
+  # 임시 파일을 생성하여 모든 버전 정보를 저장합니다.
+  temp_file=$(mktemp)
+
+  while true;
+  do
+    # Docker Hub API를 호출하여 태그 정보를 가져옵니다.
+    result=$(curl -s "https://hub.docker.com/v2/repositories/$provider/$cms/tags?page_size=100&page=$page")
+
+    # 숫자와 점(.)으로만 구성된 버전 태그를 추출하여 임시 파일에 추가합니다.
+    echo "$result" | jq -r '.results[].name' | grep -E '^[0-9]+(\.[0-9]+)*$' >> "$temp_file"
+
+    # 다음 페이지가 있는지 확인합니다.
+    next=$(echo "$result" | jq -r '.next')
+    if [ "$next" = "null" ]; then
+      break
+    fi
+    page=$((page + 1))
+  done
+
+  # 임시 파일의 내용을 버전순으로 정렬하고, 가장 구체적인 버전만 필터링하여 최종 파일에 저장합니다.
+  sort -V -u "$temp_file" | awk '
+    # 이전 라인과 현재 라인을 비교하여 덜 구체적인 버전을 필터링합니다.
+    # 예: prev="8.0", $0="8.0.0" 이면 "8.0"은 "8.0.0"의 접두사이므로 출력하지 않습니다.
+    NR > 1 {
+        if (length(prev) < length($0) && index($0, prev".") == 1) {
+            # 이전 버전이 현재 버전의 접두사인 경우 (예: 8, 8.0), 아무것도 하지 않습니다.
+        } else {
+            print prev
+        }
+    }
+    { prev = $0 }
+    END {
+        # 마지막 라인을 출력합니다.
+        if (NR > 0) print prev
+    }
+  ' > "$result_file"
+
+  # 임시 파일을 삭제합니다.
+  rm "$temp_file"
+
+done
+
+echo "모든 작업이 완료되었습니다."
