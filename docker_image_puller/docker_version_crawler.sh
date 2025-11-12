1c8594a393bfe95932f12d6d94def3d0edceee3e
wnghks7787 <wnghks7787@naver.com>
2025-11-12 15:48:23 +0900

    remove unused codes (docker_version_crawler.sh comments)

--- docker_image_puller/docker_version_crawler.sh
+++ docker_image_puller/docker_version_crawler.sh
@@ -1,36 +1,3 @@
-# #!/bin/bash
-
-# RELATIVE_PATH="../resources"
-# RESOURCES_PATH=${PWD}'/'$RELATIVE_PATH
-# FILE=$RESOURCES_PATH/"docker_hub_library.csv"
-
-# while IFS=',' read -r provider cms;
-# do
-#     echo "Provider: $provider, CMS: $cms"
-
-#     page=1
-#     result_directory=$RESOURCES_PATH"/docker_hub_library_version"
-#     result_file=$result_directory"/"$cms"_version"
-#     mkdir $result_directory
-#     touch $result_file
-#     (
-#         while true;
-#         do
-#             result=$(curl -s "https://hub.docker.com/v2/repositories/$provider/$cms/tags?page_size=100&page=$page")
-            
-
-#             # echo "$result" | jq -r '.results[].name | match("[0-9]+(\\.[0-9]+)+") | .string' >> $result_file
-#             echo "$result" | jq -r '.results[].name' | grep -E '^[0-9]+(\.[0-9]+)*$'
-
-#             next=$(echo "$result" | jq -r '.next')
-#             if [ "$next" = "null" ]; then
-#                 break
-#             fi
-#             page=$((page + 1))
-#         done
-#     ) | sort -V -u > "$result_file"
-# done < <(tail -n  +1 "$FILE") 
-
 #!/bin/bash
 
 # 리소스 파일 경로를 설정합니다.
